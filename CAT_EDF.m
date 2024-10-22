a581c02f4a8455d6b962efa2212389d37ded7a73
rgutierrez1 <rgutierrez3@mgh.harvard.edu>
2024-10-22 17:13:53 -0300

    new code for visual inspection of the data

--- CAT_EDF.m
+++ CAT_EDF.m
@@ -1,20 +1,22 @@
 % This script is intended to read EDFs files from PEUMA Sedline
 % files. 
-clear all
+clear variables
 close all
 clc
 
-% Paths (change accordingly)
 EDFpath = '~/all_data/igor_data';
-CSVpath = '/Users/jiegana/Dropbox/Respaldo_FONDEF_ID19I10345/Scripts_Prep_PEUMA2';
-SCTGpath = '/Users/jiegana/Dropbox/Respaldo_FONDEF_ID19I10345/Scripts_Prep_PEUMA2/SPECTG';
+addpath(genpath('~/Codes/eeg_general'))
+addpath(genpath(EDFpath))
+% Paths (change accordingly)
+% CSVpath = '/Users/jiegana/Dropbox/Respaldo_FONDEF_ID19I10345/Scripts_Prep_PEUMA2';
+% SCTGpath = '/Users/jiegana/Dropbox/Respaldo_FONDEF_ID19I10345/Scripts_Prep_PEUMA2/SPECTG';
 
 cd (EDFpath);
 %% Find folders with EDFs inside
 Finfo = dir;                            %Directory information
 SubF = find ([Finfo.isdir] == 1);       %Find SubFolders
 RealF = {Finfo(SubF).name};             %Folders Names
-IndexSA = strmatch('SF',RealF)';        %Indices of Folders named 'SF'
+IndexSA = strmatch('CPND-',RealF)';        %Indices of Folders named 'SF'
 SDataF = RealF(IndexSA)';               %Names of all Folders name containing 'SF'
 
 %% Find EDF files for each subject folder
@@ -23,38 +25,24 @@ for SFindex = 1:length (SDataF)
     %         hasedf(1,SFindex) = any(size(dir([[EDFpath '/' SDataF{SFindex}] '**/*.edf' ]),1));
     filelist(SFindex).files = dir(fullfile([EDFpath '/' SDataF{SFindex}], '**/*.edf'));
 end
-%% Read Time data from CSV
-cd (CSVpath);
-TAPS = readtable('times_APS.csv');
-APSSubj = table2cell(TAPS(:,1));                    %Subjects by APS
-TAPS.AlphaRelativePow = zeros(size(TAPS,1),1);
-TAPS.AlphaPeakFreq = zeros(size(TAPS,1),1);
-%% Finding APS subjects
-[APSval,APSpos]=intersect(SDataF,APSSubj);          % coincidences
-[APSdifval,APSdifpos] = setdiff(SDataF,APSSubj);    % differences
-tAPS = cell2mat(table2cell(TAPS(:,2:3)));           % Times by APS
-tAPS = rmmissing(tAPS);
-DelTrue = cell2mat(table2cell(TAPS(:,4)));          % Delirium = 1
+
 %%
-% Delirium/No Delirium counters
-% NonDel = 0;
-% YesDel = 0;
-% Main Loop
-for NumSubj = 1%:length(APSval)
+
+for NumSubj = 26:length(filelist)
     catEEG = [];
     
-    APSSubj = APSpos(NumSubj); %replacing by subjects in APS's list
+    %APSSubj = APSpos(NumSubj); %replacing by subjects in APS's list
     
-    currSubj = char(SDataF(APSSubj))
-    tt = char(currSubj);
-    currPath = filelist(APSSubj).files.folder;
-    currDel = DelTrue(NumSubj);     % Delirium. Yes = 1, No = 0.
+    currSubj = char(SDataF(NumSubj));
+    %tt = char(currSubj);
+    currPath = filelist(NumSubj).files.folder;
+    %currDel = DelTrue(NumSubj);     % Delirium. Yes = 1, No = 0.
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %     Concatenating all files for the same subject    %
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    for NumSFile = 1:length(filelist(APSSubj).files)
-        currFile = filelist(APSSubj).files(NumSFile).name;
+    for NumSFile = 1:length(filelist(NumSubj).files)
+        currFile = filelist(NumSubj).files(NumSFile).name;
         f2r = [currPath '/' currFile];
         [hdr, record] = edfreadUntilDone(f2r);
         %         File exchange function edfreadUntilDone
@@ -66,7 +54,7 @@ for NumSubj = 1%:length(APSval)
             Firsthdr = hdr;
         end
     end
-    datafilename = [currSubj '_all_EGG.mat'];
-    cd '/Users/jiegana/Desktop' % Destination folder
+    datafilename = [currSubj '_all_EEG.mat'];
+    cd '~/all_data/igor_data' % Destination folder
     save (datafilename, 'catEEG', 'Firsthdr');
 end
---
+++ CE_visual_inspection.m
@@ -0,0 +1,13 @@
+%% CE_visual_inspection
+clear variables
+clc
+
+codepath = '~/Codes/cardiac_eeg';
+datapath = '~/all_data/igor_data/catEEG';
+
+addpath(genpath(codepath))
+addpath(genpath(datapath))
+
+cd(codepath)
+
+%%
